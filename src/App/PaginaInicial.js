d6c125defec82b828dd3458681cad6a13acce360
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-07-20 10:16:26 -0400

    teste

--- src/App/PaginaInicial.js
+++ src/App/PaginaInicial.js
@@ -4,6 +4,7 @@ export default function PaginaInicial(){
         <>
             <div className="page1">
             <img src="assets/logo.png"/>
+            <b>teste</b>
             <b>ZapRecall</b>
             </div>
             <div className="button">Iniciar Recall!</div>
