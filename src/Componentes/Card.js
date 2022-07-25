170904f7d67fea55640b13fc7f56400a4433a5b8
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-07-25 15:00:33 -0400

    teste

--- src/Componentes/Card.js
+++ src/Componentes/Card.js
@@ -1,61 +1,64 @@
 import React from "react"
 import CardFechado from "./CardFechado";
-import { contador } from "./CardFechado";
-
 
 const questoes = [
     {   pergunta: "O que é JSX?",
-        resposta: "Uma extensão de linguagem do JavaScript",
-        card : false
+        resposta: "Uma extensão de linguagem do JavaScript"
     },
     {   pergunta: "O React é __",
-        resposta: "uma biblioteca JavaScript para construção de interfaces",
-        card : false
+        resposta: "uma biblioteca JavaScript para construção de interfaces"
     },
     {   pergunta: "Componentes devem iniciar com __?",
-        resposta: " letra maiúscula",
-        card : false
+        resposta: " letra maiúscula"
     },
     {   pergunta: "Podemos colocar __ dentro do JSX",
-        resposta: "expressões",
-        card : false
+        resposta: "expressões"
     },
     {   pergunta: "O ReactDOM nos ajuda __",
-        resposta: "interagindo com a DOM para colocar componentes React na mesma",
-        card : false
+        resposta: "interagindo com a DOM para colocar componentes React na mesma"
     },
     {   pergunta: "Usamos o npm para __",
-        resposta: "gerenciar os pacotes necessários e suas dependências",
-        card : false
+        resposta: "gerenciar os pacotes necessários e suas dependências"
     },
     {   pergunta: "Usamos props para __",
-        resposta: "passar diferentes informações para componentes",
-        card : false
+        resposta: "passar diferentes informações para componentes"
     },
     {   pergunta: "Usamos estado (state) para __",
-        resposta: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
-        card : false
+        resposta: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
     }
     ];
-
     
+    
+
 export default function Card (){
-  
+    
+    const [resultado, setResultado] = React.useState(0);
+    const [icons, setIcons] = React.useState("");
+        console.log(icons);
     return ( 
-         <>
+        <>
          <div className="perguntas">
          {questoes.map((objeto, index) => (
                 <div className="caixa" key={index}>
                     <CardFechado
                     arrayPergunta = {objeto.pergunta}
                     arrayResposta = {objeto.resposta}
-                    index = {index + 1}/>
+                    index = {index + 1}
+                    setResultado = {setResultado}
+                    setIcons = {setIcons}
+                    icons = {icons}
+                    />
                 </div>
              ))}
-                <div className="rodape">
-                    <p>/4 CONCLUÍDOS</p>
+            <div className="rodape">
+                 <p>{resultado}/8 CONCLUÍDOS</p>
+                 
+                 <div className="resultado">
+                   <ion-icon name={icons}></ion-icon>
                 </div>
+                
+            </div> 
             </div>
-         </>
-     )
+        </>
+    )
 }
--- src/Componentes/CardFechado.js
+++ src/Componentes/CardFechado.js
@@ -1,22 +1,46 @@
 
 import React from "react";
 let contador = 0;
+let iCones = "";
+
 
 export default function CardFechado (
     {arrayPergunta,
     arrayResposta,
-    index}){
+    index,
+    setIcons,
+    icons,
+    setResultado}){
     const [card, setCard] = React.useState(false);
     const [resposta, setResposta] = React.useState(false);
     
-        
+    function click(){
+        setCard(!card);
+        contador ++;  
+    }
+    setResultado (contador);
+    
+function vermelho(){
+    iCones = "close-circle";
+    
+}
+function amarelo(){
+    iCones = "help-circle";
+    
+}
 
+
+ 
 
 return (
-    <>{!card ? 
+
+    <>
+    {!card ? 
         <>    
-            <p> Pergunta {index}</p>
-            <ion-icon name="play-outline" onClick = {() => setCard(true)}></ion-icon>
+            <div className="caixa-fechada">
+                <p> Pergunta {index}</p>
+                <ion-icon name="play-outline" onClick = {() => setCard(true)}></ion-icon>
+            </div>
         </> : !resposta ?
         <>
             <p> {arrayPergunta}</p>
@@ -27,22 +51,24 @@ return (
         <>  
             <div className="botaoResposta">
                 <p> {arrayResposta}</p>
-                <div className="botao-resposta" onClick = {() => setCard(false)}>
-            <div className="vermelho">
+                <div className="botao-resposta" onClick = {click}>
+            <div className="vermelho" onClick = {()=>setIcons("close-circle")}>
                 <p>Não lembrei</p>
             </div>
-            <div className="amarelo">
+            <div className="amarelo" onClick = {()=>setIcons("help-circle")}>
                 <p>Quase não lembrei</p>
             </div>
 
-            <div className="verde">
-                <p>Zap!</p>
+                <div className="verde" onClick = {()=>setIcons("checkmark-circle")}>
+                    <p>Zap!</p>
+                </div>
             </div>
         </div>
-    </div>
+        
         </>
         }
-        </>            
+        </> 
+                  
     )
 }
-export {contador};
+
